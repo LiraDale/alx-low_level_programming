3946ea345f04b683e3aa1e35adac516a97cfa5fe
LiraDale <pakengmamosadi@gmail.com>
2023-07-21 08:18:52 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_nmber(int a, int b, int c);
+void Print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
